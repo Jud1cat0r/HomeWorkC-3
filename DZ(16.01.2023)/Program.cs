2f63d517c3bb936dd46dc007185458b166ae7833
Pavel Chernenkiy <gamejudicator@gmail.com>
2023-01-27 03:22:51 +0300

    first commit

---
+++ DZ(16.01.2023)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Formats.Tar;
+using System.IO;
+
+namespace DZ_16._01._2023_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckDirectory.Check();
+        }
+    }
+
+    static class CheckDirectory
+    {
+
+        static public void Check()
+        {
+            Console.WriteLine("Введите путь к директории:");
+            DirectoryInfo directoryInfo = new DirectoryInfo(Console.ReadLine());
+            FileInfo[] file = directoryInfo.GetFiles();
+            Console.WriteLine("Список файлов:");
+            foreach (var e in file)
+            {
+                Console.WriteLine($"Имя файла: {e.Name} / Размер файла: {e.Length} байт / Дата создания файла: {e.CreationTime}");
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
